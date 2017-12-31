aec838d7bfd0502781b832716b2666140b675a7c
Robert Grasboeck <es12m019@technikum-wien.at>
2017-12-31 14:57:46 +0100

    added c-file header

--- nunchuck64.c
+++ nunchuck64.c
@@ -1,3 +1,25 @@
+//=============================================================================
+// *** Nunchuk64 ***
+// Copyright (c) Robert Grasböck, All rights reserved.
+//
+// This program is free software: you can redistribute it and/or modify
+// it under the terms of the GNU General Public License as published by
+// the Free Software Foundation, either version 3 of the License, or
+// (at your option) any later version.
+//
+// This program is distributed in the hope that it will be useful,
+// but WITHOUT ANY WARRANTY; without even the implied warranty of
+// MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
+// GNU General Public License for more details.
+//
+// You should have received a copy of the GNU General Public License
+// along with this program.  If not, see <http://www.gnu.org/licenses/>.
+//=============================================================================
+/// @file   nunchuck64.c
+/// @author Robert Grasböck (robert.grasboeck@gmail.com)
+/// @date   December, 2017
+/// @brief  main
+//=============================================================================
  
 #include <inttypes.h>
 #include <avr/io.h>
