27ec34f84d7584ef8721f72d8260075b93a9d9d6
Robert Grasboeck <robert.grasboeck@gmail.com>
2018-01-07 20:12:36 +0100

    fixed reformat bug

--- i2c_master.h
+++ i2c_master.h
@@ -91,7 +91,7 @@ mode
 /**@{*/
 
 #if (__GNUC__ * 100 + __GNUC_MINOR__) < 304
-#error "This library requires AVR-GCC 3.4 or later, update to newer AVR-GCC
+#error "This library requires AVR-GCC 3.4 or later, update to newer AVR-GCC \
 compiler !"
 #endif
 
